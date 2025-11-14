d7c1e150755ae0eded50db703ed8a41fa37d2fc0
Vladimir Galunshchikov <soyayaos@gmail.com>
2025-11-14 23:05:12 +0000

    v25.11.14+230512

--- vdraft/ImplementationProperties.go
+++ vdraft/ImplementationProperties.go
@@ -1,9 +1,10 @@
 package vdraft
 
 type ImplementationProperties struct {
-	Icons      Audience  `json:"icons"`
-	Name       Cursor    `json:"name"`
-	Title      Cursor    `json:"title"`
-	Version    Default   `json:"version"`
-	WebsiteURL BlobClass `json:"websiteUrl"`
+	Description Cursor    `json:"description"`
+	Icons       Audience  `json:"icons"`
+	Name        Cursor    `json:"name"`
+	Title       Cursor    `json:"title"`
+	Version     Default   `json:"version"`
+	WebsiteURL  BlobClass `json:"websiteUrl"`
 }
