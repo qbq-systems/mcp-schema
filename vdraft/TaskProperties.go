dc366425d87da47ce6d7e95eed4bd78d72e3892a
Vladimir Galunshchikov <soyayaos@gmail.com>
2025-11-24 16:15:13 +0000

    v25.11.24+161513

--- vdraft/TaskProperties.go
+++ vdraft/TaskProperties.go
@@ -2,6 +2,7 @@ package vdraft
 
 type TaskProperties struct {
 	CreatedAt     Cursor             `json:"createdAt"`
+	LastUpdatedAt Cursor             `json:"lastUpdatedAt"`
 	PollInterval  Cursor             `json:"pollInterval"`
 	Status        AnnotationsElement `json:"status"`
 	StatusMessage Cursor             `json:"statusMessage"`
